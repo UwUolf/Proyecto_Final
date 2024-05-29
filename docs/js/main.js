4ea1b062c060a9224b3e9f975c74bde997925471
Wolf <wolfcruz143@gmail.com>
2024-05-29 12:35:37 -0600

    Buscador y pagina acerca

---
+++ docs/js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("keyup", e=>{
+
+  if (e.target.matches("#buscador")){
+
+      if (e.key ==="Escape")e.target.value = ""
+
+      document.querySelectorAll(".articulo").forEach(article =>{
+
+          article.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?article.classList.remove("filtro")
+            :article.classList.add("filtro")
+      })
+
+  }
+
+
+})
